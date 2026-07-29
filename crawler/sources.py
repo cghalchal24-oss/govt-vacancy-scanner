febd1ca28634a35bd3e51fded7a059cf0763b304
cghalchal24-oss <cghalchal24@gmail.com>
2026-07-29 15:33:58 +0530

    Create sources.py

---
+++ crawler/sources.py
@@ -0,0 +1,10 @@
+SOURCES = [
+    {
+        "name": "UPSC",
+        "url": "https://www.upsc.gov.in/examinations",
+        "job_list_selector": "div.view-content div.views-row",
+        "title_selector": "a",
+        "link_selector": "a",
+        "date_selector": ".date-display-single",
+    }
+]
